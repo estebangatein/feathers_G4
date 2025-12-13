5c253572e40bd884b50123373ac61808271589be
Esteban Gatein <esteban.gatein@gmail.com>
2025-12-13 19:28:11 +0100

    add multimodal net

---
+++ experiments/transfer_learning.py
@@ -0,0 +1,9 @@
+from transformers import AutoImageProcessor, AutoModelForImageClassification
+
+# based on EfficientNet and trained on bird species dataset
+processor_effnet = AutoImageProcessor.from_pretrained("chriamue/bird-species-classifier")
+model_effnet = AutoModelForImageClassification.from_pretrained("chriamue/bird-species-classifier", num_labels=200, ignore_mismatched_sizes=True)
+
+# based on ViT and trained on bird species dataset
+processor_vit = AutoImageProcessor.from_pretrained("Hemg/Birds-Species-classification")
+model_vit = AutoModelForImageClassification.from_pretrained("Hemg/Birds-Species-classification", num_labels=200, ignore_mismatched_sizes=True)
