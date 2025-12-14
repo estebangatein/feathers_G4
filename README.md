2075a9d642c2f913f93c606c7c95562063dfd110
Esteban Gatein <esteban.gatein@gmail.com>
2025-12-14 14:52:49 +0100

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # feathers_G4
+
+├── BEiT_pretrained.ipynb
+├── ResNet_skinny.ipynb
+├── experiments
+│   ├── architectures_scratch.py
+│   └── transfer_learning.py
+├── multimodal_approach
+│   ├── embedding-space.ipynb
+│   ├── embeddings
+│   │   ├── test_embeddings.npy
+│   │   ├── train_embeddings.npy
+│   │   └── val_embeddings.npy
+│   └── multimodal-skinny18.ipynb
+├── submissions
+│   ├── submission_beit.csv
+│   ├── submission_multimodal.csv
+│   └── submission_skinny.csv
+└── weights
+    ├── attribute_net.pth
+    ├── multimodal_resnet18_skinny.pth
+    ├── multimodal_resnet18_skinny_best.pth
+    └── resnet18Skinny.pth
--- experiments/transfer_learning.py
+++ experiments/transfer_learning.py
@@ -7,3 +7,7 @@ model_effnet = AutoModelForImageClassification.from_pretrained("chriamue/bird-sp
 # based on ViT and trained on bird species dataset
 processor_vit = AutoImageProcessor.from_pretrained("Hemg/Birds-Species-classification")
 model_vit = AutoModelForImageClassification.from_pretrained("Hemg/Birds-Species-classification", num_labels=200, ignore_mismatched_sizes=True)
+
+# based on Swin transformers and trained on ImageNet
+processor_swin = AutoImageProcessor.from_pretrained("microsoft/swin-base-patch4-window7-224")
+model_swin = AutoModelForImageClassification.from_pretrained("microsoft/swin-base-patch4-window7-224", num_labels=200, ignore_mismatched_sizes=True)
